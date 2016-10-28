2b4b724d9a66506b90395923dc775259c9df4eea
Yin-Jou Huang <leslie071564@gmail.com>
2016-10-28 11:13:07 +0900

    avoid pending because of non-existing files.

--- count_events.sh
+++ count_events.sh
@@ -7,10 +7,16 @@ base_dir4="$4"
 which_file="$5"
 file_stamp="$base_dir1$base_dir2$base_dir3$base_dir4"_"$which_file"
 
+# check if file exists.
 raw_file=/pear/share/www-uniq/v2006-2015.cf-preparation/knp/$base_dir1/$base_dir2/$base_dir3/$base_dir4/$base_dir1$base_dir2$base_dir3$base_dir4$which_file.knp.xz
+if ! [ -f $raw_file ]
+then
+    echo "$raw_file not existed."
+    exit
+fi
+
+# decompress.
 knp_file=/data/huang/temp_$file_stamp.knp
-output_file=/windroot/huang/EventCounts/$file_stamp.txt
-result_file=/windroot/huang/EventCounts/"$file_stamp"_result.txt
 n=0
 until [ $n -ge 10000 ]
 do
@@ -18,15 +24,17 @@ do
     n=$[$n+1]
     sleep 5
 done
-#echo "decompressed"
 
+# extract events from knp file.
+output_file=/windroot/huang/EventCounts/$file_stamp.txt
+result_file=/windroot/huang/EventCounts/"$file_stamp"_result.txt
 process_file_script=/home/huang/work/EventCounts/process_knp_file.py
 merge_script=/home/huang/work/EventCounts/merge.py
 $NICE python $process_file_script -i $knp_file -o $output_file -s
-#echo "processed"
 $NICE sort -k2 $output_file -o $output_file
 $NICE python $merge_script -t 2 -f $output_file -s > $result_file
-#echo "merged"
+
+# remove temp files.
 rm -f $knp_file
 rm -f $output_file
 echo "$file_stamp done"

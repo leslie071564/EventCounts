b483aecf2250025d6a189918a3bb8061d7c84d36
Yin-Jou Huang <leslie071564@gmail.com>
2016-10-30 16:18:53 +0900

    remove hard-coded paths

--- count_events.sh
+++ count_events.sh
@@ -5,10 +5,27 @@ base_dir2="$2"
 base_dir3="$3"
 base_dir4="$4"
 which_file="$5"
-file_stamp="$base_dir1$base_dir2$base_dir3$base_dir4"_"$which_file"
+
+# read from config file
+config_file=./setting.ini
+read_ini_script=./read_ini.sh
+source $read_ini_script
+read_ini $config_file
+
+raw_dir=$INI__Data_Directory__raw_dir
+knp_tmp_dir=$INI__Data_Directory__knp_tmp_dir
+result_dir=$INI__Data_Directory__result_dir
+process_file_script=$INI__Scripts__process_knp_script
+merge_script=$INI__Scripts__merge_script
+echo $raw_dir
+echo $knp_tmp_dir
+echo $result_dir
+echo $process_file_script
+echo $merge_script
 
 # check if file exists.
-raw_file=/pear/share/www-uniq/v2006-2015.cf-preparation/knp/$base_dir1/$base_dir2/$base_dir3/$base_dir4/$base_dir1$base_dir2$base_dir3$base_dir4$which_file.knp.xz
+file_stamp="$base_dir1$base_dir2$base_dir3$base_dir4"_"$which_file"
+raw_file=$raw_dir/$base_dir1/$base_dir2/$base_dir3/$base_dir4/$base_dir1$base_dir2$base_dir3$base_dir4$which_file.knp.xz
 if ! [ -f $raw_file ]
 then
     echo "$raw_file not existed."
@@ -16,7 +33,7 @@ then
 fi
 
 # decompress.
-knp_file=/data/huang/temp_$file_stamp.knp
+knp_file=$knp_temp_dir/temp_$file_stamp.knp
 n=0
 until [ $n -ge 10000 ]
 do
@@ -26,10 +43,8 @@ do
 done
 
 # extract events from knp file.
-output_file=/windroot/huang/EventCounts/$file_stamp.txt
-result_file=/windroot/huang/EventCounts/"$file_stamp"_result.txt
-process_file_script=/home/huang/work/EventCounts/process_knp_file.py
-merge_script=/home/huang/work/EventCounts/merge.py
+output_file=$result_dir/$file_stamp.txt
+result_file=$result_dir/"$file_stamp"_result.txt
 $NICE python $process_file_script -i $knp_file -o $output_file -s
 $NICE LC_ALL=C sort -k2 $output_file -o $output_file
 $NICE python $merge_script -t 2 -f $output_file -s > $result_file

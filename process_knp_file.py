da704ddf23acb91ad1870b11863a9e4a27decdcd
Yin-Jou Huang <leslie071564@gmail.com>
2016-11-01 16:54:33 +0900

    fix bug of compound noun

--- process_knp_file.py
+++ process_knp_file.py
@@ -14,8 +14,9 @@ knp = KNP()
 def print_events(knp_file, to_file, if_debug=False, print_sid=False):
     if not os.path.isfile(knp_file):
         return False
-    data = ""
     TO_FILE = open(to_file, 'wb')
+
+    data = ""
     sent_count = 0
     now_sid = ""
     for line in iter(open(knp_file, 'r').readline, ""):
@@ -26,7 +27,7 @@ def print_events(knp_file, to_file, if_debug=False, print_sid=False):
         if line.strip() == "EOS":
             try:
                 result = knp.result(data.decode('utf-8'))
-                events_of_sent = process_sent(result)
+                events_of_sent = process_sent(result, if_debug=bool(if_debug))
                 if now_sid:
                     events_of_sent = ["1 %s %s" % (x, now_sid) for x in events_of_sent]
                 for ev in events_of_sent:
@@ -41,7 +42,7 @@ def print_events(knp_file, to_file, if_debug=False, print_sid=False):
             data = ""
 
 
-def process_sent(result):
+def process_sent(result, if_debug=False):
     ALL_EVENTS = []
     for tag_id, tag in enumerate(result.tag_list()):
         dep_cases = re.findall(ur"<係:(.*?)格>", tag.fstring)
@@ -56,15 +57,20 @@ def process_sent(result):
         # check if OK.
         if u"<用言:動>" not in pred.fstring:
             continue
+        # check if OK.
         prev_tag = result.tag_list()[tag_id - 1] if tag_id else None
         arg = get_noun_rep(tag, prev_tag)
         if arg:
             pred_str = re.search(ur"<用言代表表記:(.*?)>", pred.fstring).group(1)
             event_rep = "%s-%s-%s" % (arg, case, pred_str)
             ALL_EVENTS.append(event_rep)
+    if if_debug and ALL_EVENTS:
+        sys.stderr.write(print_original_sentence(result) + '\n')
+        sys.stderr.write(" ".join(ALL_EVENTS) + '\n')
     return ALL_EVENTS
 
-def print_original_sentence(knp_result):
+def print_original_sentence(result, delimiter=""):
+    return delimiter.join([x.midasi for x in result.mrph_list()])
     pass
 
 def get_noun_rep(this_tag, prev_tag):
@@ -72,12 +78,12 @@ def get_noun_rep(this_tag, prev_tag):
     this_repname = this_tag.repname.split('+')
     if not this_repname:
         return None
-    if len(this_repname) == 1 and len(this_repname[0].split('/')[0]) == 0:
+    if len(this_repname) == 1 and len(this_repname[0].split('/')[0]) == 1:
         flag = True
     else:
         flag = False
     
-    prev_mrph = prev_tag.mrph_list()[-1].repname
+    prev_mrph = prev_tag.mrph_list()[-1]
     for mrph in this_tag.mrph_list():
         if mrph.repname not in this_repname:
             prev_mrph = mrph
@@ -85,14 +91,17 @@ def get_noun_rep(this_tag, prev_tag):
         mrph_index = this_repname.index(mrph.repname)
         if mrph.hinsi == u"特殊":
             return None
-        if mrph.bunrui in [u"数詞", u"人名"]:
-            this_repname[mrph_index] = "[%s]" % (mrph.bunrui)
+        this_repname[mrph_index] = replace_by_category(mrph)
         if flag:
             if u"<複合←>" in mrph.fstring or this_mrph.hinsi == u"接尾辞":
-                this_repname.append(prev_mrph)
+                this_repname.insert(0, replace_by_category(prev_mrph))
                 break
     return "+".join(this_repname)
 
+def replace_by_category(mrph):
+    if mrph.bunrui in [u"数詞", u"人名", u"地名"]:
+        return "[%s]" % mrph.bunrui
+    return mrph.repname
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
